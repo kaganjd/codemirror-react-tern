9ad0f5fd669c0cc696f14b787ecd7ce11c759412
Jen Kagan <kaganjd@gmail.com>
2017-08-18 10:21:54 -0400

    remove scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import Editor from 'components/editor';
 
 require('editor.scss');
-require('../node_modules/codemirror/addon/tern/tern.scss');
 
 class App extends React.Component {
   render() {
